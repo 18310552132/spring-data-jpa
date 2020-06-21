701da66a8194dbba5ef6fb3236a3f1a78c46d45e
zhang <759005617@qq.com>
2020-06-21 18:31:42 +0800

    第一次提交

---
+++ src/main/java/org/springframework/data/jpa/domain/support/demo2.java
@@ -0,0 +1,27 @@
+package org.springframework.data.jpa.domain.support;
+
+
+
+/**
+ * @author shkstart
+ * @create 2020-06-21 17:54
+ */
+public class demo2 {
+    public static void main(String[] args) {
+
+        String[] s = new String[6];
+        s[0] = "张三";
+        s[1] ="张三";
+        s[2] ="张三";
+        s[3] ="张三";
+        s[4] ="张三";
+        s[5] ="张三";
+        s[6] ="张三";
+
+        System.out.println(s.length);
+
+    }
+
+
+
+}
